72361cf1281b74e53e7975198d32545084ffde21
SunRiseMedia <sidharthpb7@gmail.com>
2026-06-01 14:35:57 +0200

    fix presentation preview url prefix

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import { schemaTypes } from "./sanity/schemaTypes";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-const previewUrl = process.env.SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000";
+const previewUrl = process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000";
 
 export default defineConfig({
   name: "dental-clinic-template",
